2f483013f128b6bf1dba68d8dfc36eae99641cbd
thoccsupply <99397782+thoccsupply@users.noreply.github.com>
2025-09-25 20:06:55 +1000

    Init QMK firmware file
    Setting up general files for QMK firmware

--- keyboards/thoccsupply/exnumpad/keymaps/default/keymap.c
+++ keyboards/thoccsupply/exnumpad/keymaps/default/keymap.c
@@ -1,54 +1,55 @@
-// Copyright 2021 QMK / NachoxMacho
-// SPDX-License-Identifier: GPL-2.0-or-later
+/* SPDX-License-Identifier: GPL-2.0-or-later */
 
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /*
-     * ┌───┌───┬───┬───┬───┐
-     * │ ` │Esc│Tab│MO1│Bsp│
-     * ├───┼───┼───┼───┼───┤
-     * │ , │Num│ / │ * │ - │
-     * ├───┼───┼───┼───┼───┤
-     * │Gui│ 7 │ 8 │ 9 │   │
-     * ├───┼───┼───┼───┤ + │
-     * │Alt│ 4 │ 5 │ 6 │   │
-     * ├───┼───┼───┼───┼───┤
-     * │Shi│ 1 │ 2 │ 3 │   │
-     * ├───┼───┴───┼───┤Ent│
-     * │Ctr│ 0     │ . │   │
-     * └───┴───────┴───┴───┘
-     */
-    [0] = LAYOUT_numpad_6x5(
-        KC_GRV,  KC_ESC, KC_TAB,  MO(1),   KC_BSPC,
-        KC_COMM, KC_NUM, KC_PSLS, KC_PAST, KC_PMNS,
-        KC_LGUI, KC_P7,  KC_P8,   KC_P9,
-        KC_LALT, KC_P4,  KC_P5,   KC_P6,   KC_PPLS,
-        KC_LSFT, KC_P1,  KC_P2,   KC_P3,
-        KC_LCTL, KC_P0,           KC_PDOT, KC_PENT
-    ),
 
-    /*
-     * ┌───┌───┐───┬───┬───┐
-     * │ ` │Rst│Tab│MO1│Bsp│
-     * ├───┼───┘───┼───┼───┤
-     * │ , │Num│ / │ * │ - │
-     * ├───┼───┼───┼───┼───┤
-     * │Gui│Hom│ ↑ │PgU│   │
-     * ├───┼───┼───┼───┤ + │
-     * │Alt│ ← │   │ → │   │
-     * ├───┼───┼───┼───┤───┤
-     * │Shi│End│ ↓ │PgD│   │
-     * ├───┼───┴───┼───┤Ent│
-     * │Ctr│Insert │Del│   │
-     * └───┴───────┴───┘───┘
-     */
-    [1] = LAYOUT_numpad_6x5(
-        _______, QK_BOOT,   _______, _______, _______,
-        _______, _______, _______, _______, _______,
-        _______, KC_HOME, KC_UP,   KC_PGUP,
-        _______, KC_LEFT, XXXXXXX, KC_RGHT, _______,
-        _______, KC_END,  KC_DOWN, KC_PGDN,
-        _______, KC_INS,           KC_DEL,  _______
-    )
+	[0] = LAYOUT(
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+	),
+
+	[1] = LAYOUT(
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+	),
+
+	[2] = LAYOUT(
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+	),
+
+	[3] = LAYOUT(
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+	),
+
 };
+
+
+/* `ENCODER_MAP_ENABLE = yes` must be added to the rules.mk at the KEYMAP level. See QMK docs. */
+/* Remove the following code if you do not enable it in your keymap (e.g. default keymap). */
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+	[0] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
+	[1] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
+	[2] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
+	[3] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
+};
+#endif
