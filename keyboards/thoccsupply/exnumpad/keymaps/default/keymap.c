39fc43a7a117850d2f0ea448d4f6f0e70b625fac
thoccsupply <99397782+thoccsupply@users.noreply.github.com>
2025-11-01 23:15:49 +1100

    Beta firmware update
    Correct encoder pin number, created VIA firmware and layout JSON. Added Lighting menu support. Firmware tested working. Swapped Copy keycode to CAPS due to not supported on Win Sys

--- keyboards/thoccsupply/exnumpad/keymaps/default/keymap.c
+++ keyboards/thoccsupply/exnumpad/keymaps/default/keymap.c
@@ -15,7 +15,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * |-------+------+------+------+------| PLUS |------|
     * | HOME  | END  |  4   |  5   |  6   |      | DEL  |
     * |-------+------+------+------+------|------|------|
-    * | PGUP  |      |  1   |  2   |  3   |      |COPY  |
+    * | PGUP  |      |  1   |  2   |  3   |      |Caps  |
     * |-------+ ENT  +------+------+------| ENT  |------|
     * | PGDN  |      |      0      |  .   |      |MO(1) |
     * `-------------------------------------------------'
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                     KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
        KC_MUTE,     KC_P7,   KC_P8,   KC_P9,   KC_PPLS,   KC_INS,
   KC_HOME, KC_END,  KC_P4,   KC_P5,   KC_P6,              KC_DEL,
-  KC_PGUP, KC_PENT, KC_P1,   KC_P2,   KC_P3,   KC_PENT,   KC_COPY,
+  KC_PGUP, KC_PENT, KC_P1,   KC_P2,   KC_P3,   KC_PENT,   KC_CAPS,
   KC_PGDN,                   KC_P0,        KC_PDOT,       MO(1)
  ),
  [1] = LAYOUT_default(
@@ -76,10 +76,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // clang-format on
 
 #if defined(ENCODER_MAP_ENABLE)
-const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-	[0] = { ENCODER_CCW_CW(KC_VOLU, KC_VOLD) },
-	[1] = { ENCODER_CCW_CW(KC_PGUP, KC_PGDN) },
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+	[0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+	[1] = { ENCODER_CCW_CW(KC_PGDN, KC_PGUP) },
 	[2] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
-	[3] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
+	[3] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) }
 };
 #endif
